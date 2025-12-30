54dbeedc697c06d6e06faf59c0b5dcc28f26cefb
Nagisa <s1144101@ms1.nihs.tp.edu.tw>
2025-12-30 20:33:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/F8DD8805-5E89-45C8-B9CF-55CD4FB84D5D.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/IMG_1552.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Nagisa",
 	bio: "play pc:>",
 	links: [
